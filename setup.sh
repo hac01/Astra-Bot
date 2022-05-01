4b794e92d4d138826d6847adac040e22342bd3dc
hac <70646122+hac01@users.noreply.github.com>
2022-05-01 17:25:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "It may take some time to complete"
 sudo apt update
-sudo apt install nmap
-sudo apt install golang-go
+sudo apt install -y nmap
+sudo apt install -y golang-go
 go install -v github.com/OWASP/Amass/v3/...@master
 sudo cp ~/go/bin/amass /usr/bin
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 sudo cp ~/go/bin/subfinder /usr/bin
+pip install -r requerments.txt
