c3f03b9c371b237d7380bc708e3798b30c4f4729
hac <70646122+hac01@users.noreply.github.com>
2022-05-01 15:47:59 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt install nmap
+sudo apt install golang-go
+go install -v github.com/OWASP/Amass/v3/...@master
+sudo cp ~/go/bin/amass /usr/bin
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+sudo cp ~/go/bin/subfinder /usr/bin
