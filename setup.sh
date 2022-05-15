d7e3344f18142ed321cfe5a15f95ea11a51bb115
hac <70646122+hac01@users.noreply.github.com>
2022-05-16 02:10:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,4 +7,4 @@ go install -v github.com/OWASP/Amass/v3/...@master
 sudo cp ~/go/bin/amass /usr/bin
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 sudo cp ~/go/bin/subfinder /usr/bin
-pip install -r requerments.txt
+pip install -r requirements.txt
